f61896fec75e7801b72bc16b39524a0410cda0a4
JeffMatson <JeffMatson@users.noreply.github.com>
2024-10-09 22:50:58 -0400

    add some file check utilities

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,12 @@
+import type { SupportedCryptoAlgorithms } from "bun";
 import { fileTypeFromBuffer } from "file-type";
 import isSvg from "is-svg";
+
+type ReleaseCheckOptions = {
+  md5?: boolean;
+  sha1?: boolean;
+  sha256?: boolean;
+};
 
 export async function getImageType(buffer: Uint8Array) {
   const type = await fileTypeFromBuffer(buffer);
@@ -32,4 +39,36 @@ export async function getImageType(buffer: Uint8Array) {
 
   console.error("Failed to determine image type");
   return false;
+}
+
+export function getFileHash(
+  buffer: Uint8Array,
+  algorithm: SupportedCryptoAlgorithms
+) {
+  const hasher = new Bun.CryptoHasher(algorithm);
+  hasher.update(buffer);
+  return hasher.digest("hex");
+}
+
+export async function checkRelease(
+  buffer: Uint8Array,
+  options: ReleaseCheckOptions
+) {
+  const results: {
+    md5?: string;
+    sha1?: string;
+    sha256?: string;
+  } = {};
+
+  if (options.md5) {
+    results.md5 = getFileHash(buffer, "md5");
+  }
+
+  if (options.sha1) {
+    results.sha1 = getFileHash(buffer, "sha1");
+  }
+
+  if (options.sha256) {
+    results.sha256 = getFileHash(buffer, "sha256");
+  }
 }
