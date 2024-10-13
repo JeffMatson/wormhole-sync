786413a26350a99df810d40d39d0b04b2d0ea16b
JeffMatson <JeffMatson@users.noreply.github.com>
2024-10-13 19:43:17 -0400

    add stats upsert

---
+++ src/db/plugin-stats.ts
@@ -0,0 +1,20 @@
+import type { Prisma } from "@prisma/client";
+import prismaClient from "./client";
+
+// @TODO: dodge that any
+export async function updateDotOrgPluginStats(pluginId: number, stats: any) {
+  try {
+    const result = await prismaClient.dotOrgPluginStats.upsert({
+      where: { pluginId },
+      update: stats,
+      create: {
+        pluginId,
+        ...stats,
+      },
+    });
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
