465ffdac7d67dfc604aca8a8963d940bd51195a8
JeffMatson <JeffMatson@users.noreply.github.com>
2024-10-15 13:30:26 -0400

    oops forgot to save

--- src/queues/resource.ts
+++ src/queues/resource.ts
@@ -1,11 +1,11 @@
 import PQueue from "p-queue";
-import config from "../config";
+import { WormholeSyncConfig } from "~/config";
 import CLI from "../cli";
 
 let completed = 0;
 
 const ResourceQueue = new PQueue({
-  concurrency: config.concurrency.plugins,
+  concurrency: WormholeSyncConfig.concurrency.plugins,
 });
 
 CLI.trackQueue("resource");
